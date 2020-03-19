2d92f665725525bbeaf760df4bf772a77e4559cb
redass12 <redataraoui1@gmail.com>
2020-03-19 23:21:14 +0100

    first deploy

---
+++ precache-manifest.4100f2c316c5ebdb211faaf02ff0ce97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add4df80a6e3558cda3af9eda303cfef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7720845606b406f6b4fa",
+    "url": "/static/css/main.ebe9b87b.chunk.css"
+  },
+  {
+    "revision": "97238a0ab5505e7e2fc7",
+    "url": "/static/js/2.9f867039.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.9f867039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7720845606b406f6b4fa",
+    "url": "/static/js/main.c32cb6b4.chunk.js"
+  },
+  {
+    "revision": "bfeef93ebf0429c2178a",
+    "url": "/static/js/runtime-main.fbeae3c0.js"
+  }
+]);
